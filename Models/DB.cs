00c917b83a089d57f8d19db2691da510325ce88a
“Marco” <marcantonio1963@libero.it>
2024-03-01 15:21:08 +0100

    Aggiungere i file di progetto.

---
+++ Models/DB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace PoliziaMunicipale.Models
+{
+    public class DB
+    {
+        public static string connectionString = "server=DESKTOP-NHB4AFL\\SQLEXPRESS; Initial Catalog=PoliziaMunicipale; Integrated Security=true; TrustServerCertificate=true";
+        public static SqlConnection conn = new SqlConnection(connectionString);
+    }
+
+}
---
+++ Models/PuntiSaldo.cs
@@ -0,0 +1,16 @@
+﻿namespace PoliziaMunicipale.Models
+{
+    public class PuntiSaldo
+    {
+        public string Cognome { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataViolazione { get; set; }
+
+        public decimal Importo { get; set; }
+
+        public int DecurtamentoPunti { get; set; }
+
+    }
+}
